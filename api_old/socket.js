42652bd2ac8cf511c283c8c5172543913d0aebf0
mrght12 <mrght1234@gmail.com>
2026-05-22 20:59:10 +0300

    socket.js

--- api_old/socket.js
+++ api_old/socket.js
@@ -1,7 +1,7 @@
 const { Server } = require("socket.io");
 
-const users = new Map(); // socketId -> {username, avatar}
-const groups = new Map(); // groupId -> {name, type: 'group'/'channel', members: [], messages: []}
+const users = {};
+const groups = [];
 
 module.exports = (req, res) => {
   if (!res.socket.server.io) {
@@ -12,76 +12,64 @@ module.exports = (req, res) => {
 
     io.on("connection", (socket) => {
       
-      // Регистрация пользователя
+      // Регистрация
       socket.on("register", (data) => {
-        users.set(socket.id, {
-          email: data.email,
-          username: data.username,
-          avatar: data.avatar || "",
-          socketId: socket.id
-        });
-        socket.emit("registered", { userId: socket.id });
+        users[socket.id] = {
+          username: data.username || "User",
+          email: data.email || "",
+          avatar: ""
+        };
+        socket.emit("registered", users[socket.id]);
       });
 
       // Вход
-      socket.on("login", (email) => {
-        const user = Array.from(users.values()).find(u => u.email === email);
-        if (user) {
-          socket.emit("loginSuccess", user);
-        } else {
-          socket.emit("loginError", "Пользователь не найден");
-        }
-      });
-
-      // Обновление профиля
-      socket.on("updateProfile", (data) => {
-        const user = users.get(socket.id);
-        if (user) {
-          user.username = data.username || user.username;
-          user.avatar = data.avatar || user.avatar;
-          users.set(socket.id, user);
-          socket.emit("profileUpdated", user);
-        }
-      });
-
-      // Создание группы/канала
-      socket.on("createGroup", (data) => {
-        const groupId = Date.now().toString();
-        groups.set(groupId, {
-          id: groupId,
-          name: data.name,
-          type: data.type, // 'group' или 'channel'
-          creator: users.get(socket.id)?.username || "Admin",
-          members: [socket.id],
-          messages: [],
-          avatar: data.avatar || ""
+      socket.on("login", (data) => {
+        socket.emit("loginSuccess", {
+          username: data.email || "User",
+          email: data.email || ""
         });
-        io.emit("groupCreated", groups.get(groupId));
+      });
+
+      // Создание группы
+      socket.on("createGroup", (data) => {
+        const group = {
+          id: Date.now().toString(),
+          name: data.name,
+          type: data.type,
+          messages: []
+        };
+        groups.push(group);
+        io.emit("groupCreated", group);
+      });
+
+      // Получить список групп
+      socket.on("getGroups", () => {
+        socket.emit("groupsList", groups);
       });
 
       // Присоединиться к группе
       socket.on("joinGroup", (groupId) => {
         socket.join(groupId);
-        const group = groups.get(groupId);
-        if (group && !group.members.includes(socket.id)) {
-          group.members.push(socket.id);
+        const group = groups.find(g => g.id === groupId);
+        if (group) {
+          socket.emit("groupMessages", group.messages);
         }
-        socket.emit("groupMessages", group?.messages || []);
       });
 
-      // Отправка в группу
+      // Отправить сообщение в группу
       socket.on("groupMessage", (data) => {
-        const user = users.get(socket.id);
+        const user = users[socket.id] || { username: "User" };
         const msg = {
           id: Date.now().toString(),
           text: data.text,
-          username: user?.username || "Аноним",
-          avatar: user?.avatar || "",
-          time: getTime(),
-          groupId: data.groupId
+          username: user.username,
+          time: new Date().toLocaleTimeString("ru-RU", {
+            hour: "2-digit",
+            minute: "2-digit"
+          })
         };
         
-        const group = groups.get(data.groupId);
+        const group = groups.find(g => g.id === data.groupId);
         if (group) {
           group.messages.push(msg);
         }
@@ -89,37 +77,9 @@ module.exports = (req, res) => {
         io.to(data.groupId).emit("newGroupMessage", msg);
       });
 
-      // Личное сообщение
-      socket.on("privateMessage", (data) => {
-        const user = users.get(socket.id);
-        const msg = {
-          id: Date.now().toString(),
-          text: data.text,
-          username: user?.username || "Аноним",
-          avatar: user?.avatar || "",
-          time: getTime(),
-          senderId: socket.id
-        };
-        
-        // Отправляем получателю
-        io.to(data.to).emit("newPrivateMessage", msg);
-        // И себе
-        socket.emit("newPrivateMessage", { ...msg, isMine: true });
-      });
-
-      socket.on("disconnect", () => {
-        users.delete(socket.id);
-      });
     });
 
     res.socket.server.io = io;
   }
   res.end();
 };
-
-function getTime() {
-  return new Date().toLocaleTimeString("ru-RU", {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-}
